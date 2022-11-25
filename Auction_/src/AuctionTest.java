97aaac8b75028eca84078b2a03dc9442614504da
cccdb2022 <98005783+cccdb2022@users.noreply.github.com>
2022-11-25 15:49:39 -0600

    Update AuctionTest.java

--- Auction_/src/AuctionTest.java
+++ Auction_/src/AuctionTest.java
@@ -11,10 +11,15 @@ public class AuctionTest {
 		
 		AuctionManagement auc = new AuctionManagement();
 	
-		auc.listForAuction("1001", "106", 200.0, 460.5, 211, "1", "11/23/2022", "12/06/2022", "2");
+		auc.listForAuction("1002", "107", 200.0, 460.5, 211, "1", "11/23/2022", "12/06/2022", "2");
 		//auc.placeBid("1006", "6", 300);
 		//auctionId, userId, param, newVal
 		//auc.updateAuction("1007", "2", "expireTime", "12/11/2022");
+		auc.addToWatchlist("107", "1");
+		auc.addToWatchlist("107", "2");
+		auc.addToWatchlist("107", "3");
+		auc.addToWatchlist("107", "4");
+		auc.addToWatchlist("107", "5");
 		
 	}
 }

efe5bd7f0de27003ebef89e98795bfffea26f1c3
cccdb2022 <98005783+cccdb2022@users.noreply.github.com>
2022-11-26 14:41:18 -0600

    clean code

--- Payment/src/main/java/PaymentTest.java
+++ Payment/src/main/java/PaymentTest.java
@@ -10,8 +10,18 @@ public class PaymentTest {
 	public static void main(String[] args) {
 		
 		PaymentManagement pm = new PaymentManagement();
-		//pm.insertToBankBalance("1001", 100);
-		pm.makePayment("1002", 30);
+		// basic test case
+		pm.insertToBankBalance("1001", 100);
+		pm.makePayment("1001", 30.8);
+		
+		// edge test case - makepayment with the userId that does not exist
+		// show error
+		pm.makePayment("1008", 30.8); 
+		
+		// edge test case - currentAmount < paymentAmount
+		// currentAmount will not change
+		pm.makePayment("1001", 1000);
+		
 		
 	}
 }

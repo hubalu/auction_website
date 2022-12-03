98d63a5c8ab6aa47e39a3afdaa3d95ab67486ecc
cccdb2022 <98005783+cccdb2022@users.noreply.github.com>
2022-12-02 20:25:21 -0600

    del
    del

--- Payment/src/main/java/app/rmiManagement/RemotePaymentManagement.java
+++
@@ -1,9 +0,0 @@
-package app.rmiManagement;
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-import java.util.List;
-
-public interface RemotePaymentManagement extends Remote {
-	 boolean insertToBankBalance(String userId, int initialAmount) throws RemoteException;
-	 boolean makePayment(String userId, double amount) throws RemoteException;
-}
